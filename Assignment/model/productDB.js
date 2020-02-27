aae7a6e8ea3b1ab816892dc3c8c6f222dc0b65d7
unknown <suhhee1011@gmail.com>
2020-02-27 15:40:45 -0500

    before start assignment2

--- Assignment/model/productDB.js
+++ Assignment/model/productDB.js
@@ -2,13 +2,13 @@ const productDB = {
 
     fakeDB: [],
     init() {
-        this.fakeDB.push({ name: "Carhartt Men's Acrylic Watch Hat", price: 16.99, source: "Carhartt Men's Acrylic Watch Hat.PNG", category: "Men's Clothes", Bestseller: "yes" });
-        this.fakeDB.push({ name: "2-pack Tech Stretch Racerback Tank Top", source: "top.PNG", price: 24, category: "Woman's Clothes", Bestseller: "yes" });
-        this.fakeDB.push({ name: "ALONG FIT Yoga Pants with Side Pockets", price: 25.95, source: "leggings.PNG", category: "Woman's Clothes", Bestseller: "yes" });
-        this.fakeDB.push({ name: "20% Vitamin C Serum - 60 ml ", price: 23.69, source: "vitaminc.PNG", category: "Beauty and personal care", Bestseller: "no" });
-        this.fakeDB.push({ name: "Smart speaker with Alexa", price: 99.99, source: "speaker.PNG", category: "Home and Kitchen", Bestseller: "yes" });
-        this.fakeDB.push({ name: "FOSHIO Car Vinyl Wrap Window Tint Tools Kit", price: 22.99, source: "cartoolkit.PNG", category: "Automotive", Bestseller: "yes" });
-        this.fakeDB.push({ name: "UNO Card Game", price: 6.93, source: "uno.PNG", category: "Toys and Games", Bestseller: "no" });
+        this.fakeDB.push({ name: "Carhartt Men's Acrylic Watch Hat", price: 16.99, source: "Carhartt Men's Acrylic Watch Hat.PNG", category: "Men's Clothes", Bestseller: true });
+        this.fakeDB.push({ name: "2-pack Tech Stretch Racerback Tank Top", source: "top.PNG", price: 24, category: "Woman's Clothes", Bestseller: true});
+        this.fakeDB.push({ name: "ALONG FIT Yoga Pants with Side Pockets", price: 25.95, source: "leggings.PNG", category: "Woman's Clothes", Bestseller: true });
+        this.fakeDB.push({ name: "20% Vitamin C Serum - 60 ml ", price: 23.69, source: "vitaminc.PNG", category: "Beauty and personal care", Bestseller: true });
+        this.fakeDB.push({ name: "Smart speaker with Alexa", price: 99.99, source: "speaker.PNG", category: "Home and Kitchen", Bestseller: false });
+        this.fakeDB.push({ name: "Wrap Window Tint Tools Kit", price: 22.99, source: "cartoolkit.PNG", category: "Automotive", Bestseller: false });
+        this.fakeDB.push({ name: "UNO Card Game", price: 6.93, source: "uno.PNG", category: "Toys and Games", Bestseller: true });
 
     },
     getAllProducts() {
@@ -19,7 +19,7 @@ const productDB = {
         let count = 0;
         for (let i = 0; i < this.fakeDB.length; i++) {
 
-            if (this.fakeDB[i].Bestseller == "yes") {
+            if (this.fakeDB[i].Bestseller ==true &&count<4) {
 
                 fakeDB2.push({ name: this.fakeDB[i].name, price: this.fakeDB[i].price, source: this.fakeDB[i].source, category: this.fakeDB[i].category, Bestseller: this.fakeDB[i].Bestseller });
                 count++;
@@ -32,12 +32,14 @@ const productDB = {
     getCategories() {
         let fakeDB3 = [];
         let count = 1;
-        let check = true;
+       
         fakeDB3.push({ name: this.fakeDB[0].name, price: this.fakeDB[0].price, source: this.fakeDB[0].source, category: this.fakeDB[0].category, Bestseller: this.fakeDB[0].Bestseller });
         for (let i = 1; i < this.fakeDB.length; i++) {
+            let check = false;
             for (let j = 0; j < fakeDB3.length; j++) {
-                if (this.fakeDB[i].category != fakeDB3[j].category) {
-                    check = false;
+                if (this.fakeDB[i].category == fakeDB3[j].category) {
+                    check = true;
+                
                 }
             }
             if (!check && count < 4) {
